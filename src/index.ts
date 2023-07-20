d11c557e596bc92d6e98870d3d77c8887176940b
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-19 20:26:47 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,18 @@ import pkg from '../package.json';
 
 const { log } = console;
 
-program.name('stripe-migrate').description(pkg.description).version(pkg.version);
+program
+  .name('stripe-migrate')
+  .description(pkg.description)
+  .version(pkg.version);
 
 program
   .command('x')
   .description('y')
   .argument('<command>', 'command to generate')
-  .option(
-    '--z <z>',
-    'z desc',
-    'default'
-  )
+  .option('--z <z>', 'z desc', 'default')
   .action(async (code: string, options: { language: string }) => {
-    // do a thing
+    log(chalk.red('test'));
   });
 
 program.parse();
