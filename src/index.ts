e6b0d2f9341b993b5ff51f5428eb23a9bfeb5a88
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-22 13:16:09 -0400

    Fix CLI

--- src/index.ts
+++ src/index.ts
@@ -10,42 +10,43 @@ import { migrateWebhooks } from './lib/webhooks';
 program
   .name('stripe-migrate')
   .description(pkg.description)
-  .version(pkg.version);
+  .version(pkg.version)
+  .option('--from <from>', 'Stripe secret key from the old account', undefined)
+  .option('--to <to>', 'Stripe secret key from the new account', undefined)
+  .action(async ({ from, to }) => {
+    console.log(chalk.green('Validating Stripe keys...'));
+    if (!from) {
+      console.log(chalk.red('<from> argument is required'));
+      return;
+    }
 
-program
-  .command('x')
-  .description('y')
-  .argument('<command>', 'command to generate')
-  .option('--z <z>', 'z desc', 'default')
-  .action(async () => {
+    if (!to) {
+      console.log(chalk.red('<to> argument is required'));
+      return;
+    }
+
     console.log(chalk.green('Creating Stripe instances...'));
 
-    if (!process.env.OLD_STRIPE_SECRET_KEY) {
-      throw new Error('OLD_STRIPE_SECRET_KEY is required');
+    const oldStripe = new Stripe(from, { apiVersion: '2022-11-15' });
+    const newStripe = new Stripe(to, { apiVersion: '2022-11-15' });
+
+    try {
+      console.log(chalk.green('Migrating plans...'));
+      await migratePlans(oldStripe, newStripe);
+
+      console.log(chalk.green('Migrating coupons...'));
+      await migrateCoupons(oldStripe, newStripe);
+
+      console.log(chalk.green('Migrating customers...'));
+      await migrateSubscriptions(oldStripe, newStripe);
+
+      console.log(chalk.green('Migrating webhooks...'));
+      await migrateWebhooks(oldStripe, newStripe);
+    } catch (error) {
+      const message = error instanceof Error ? error.message : `${error}`;
+
+      console.log(chalk.red(message));
     }
-
-    if (!process.env.NEW_STRIPE_SECRET_KEY) {
-      throw new Error('NEW_STRIPE_SECRET_KEY is required');
-    }
-
-    const oldStripe = new Stripe(process.env.OLD_STRIPE_SECRET_KEY, {
-      apiVersion: '2022-11-15',
-    });
-    const newStripe = new Stripe(process.env.NEW_STRIPE_SECRET_KEY, {
-      apiVersion: '2022-11-15',
-    });
-
-    console.log(chalk.green('Migrating plans...'));
-    await migratePlans(oldStripe, newStripe);
-
-    console.log(chalk.green('Migrating coupons...'));
-    await migrateCoupons(oldStripe, newStripe);
-
-    console.log(chalk.green('Migrating customers...'));
-    await migrateSubscriptions(oldStripe, newStripe);
-
-    console.log(chalk.green('Migrating webhooks...'));
-    await migrateWebhooks(oldStripe, newStripe);
   });
 
 program.parse();
